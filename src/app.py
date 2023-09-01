897c9935996f664bc3d29dd71396ada44ea17ccf
MrCabbageStick <mrcabbagestick@gmail.com>
2023-09-01 18:25:51 +0200

    Added database handler

--- src/app.py
+++ src/app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, redirect
+from modules.databaseHandler import DatabaseHandler
+from modules.paths import DATABASE
 
 app = Flask(__name__)
 
+database = DatabaseHandler(DATABASE)
 
 @app.route("/")
 def homePage():
     return render_template("gallery.html")
 
+@app.route("/users")
+def usersPage():
+    return jsonify(database.getUsers())
+
+@app.route("/addUser")
+def addUserPage():
+    print(database.addUser("osk", "Ośk", "lubie_jelenie", True))
+    return redirect("/users")
+
 
 
 if __name__ == "__main__":
+    database.connect()
     app.run()
---
+++ src/modules/databaseHandler.py
@@ -0,0 +1,81 @@
+import sqlite3
+from typing import Self
+from enum import Enum
+from datetime import datetime
+import bcrypt
+from modules.functions import getFormattedTime, debugPrint
+import json
+
+
+class DatabaseHandlerMessages:
+    SUCCESS = "Success"
+    CONNECTION_NOT_ESTABLISHED = "Connection not established"
+    USER_ALREADY_EXISTS = "User already exists"
+    SQLITE_ERROR = "Internal error occured"
+
+
+class DatabaseHandler:
+
+    connection: sqlite3.Connection = None
+    cursor: sqlite3.Cursor = None
+
+    def __init__(self, path: str) -> None:
+        
+        self.path = path
+        print(f"{self.path=}")
+
+    
+    def connect(self) -> Self:
+
+        self.connection = sqlite3.connect(self.path, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+
+        return self
+    
+    
+    def close(self) -> Self:
+
+        self.connection.close()
+        self.connection = None
+        self.cursor = None
+
+        return self
+    
+
+    def addUser(self, name: str, displayName: str, password: str, isOwner: bool = False) -> tuple[bool, DatabaseHandlerMessages]:
+        """Returns a tuple: (success, message)"""
+
+        if not (self.connection and self.cursor):
+            return (False, DatabaseHandlerMessages.CONNECTION_NOT_ESTABLISHED)
+
+        if self.cursor.execute(f"SELECT name FROM users WHERE name='{name}'").rowcount > 0:
+            return (False, DatabaseHandlerMessages.USER_ALREADY_EXISTS)
+        
+        password = password.encode()
+        password = bcrypt.hashpw(password, bcrypt.gensalt())
+        
+        try:
+            self.cursor.execute(f'INSERT INTO users VALUES (NULL, "{name}", "{displayName}", "{password}", "{getFormattedTime(datetime.now())}", {isOwner})')
+            self.connection.commit()
+
+            return (True, "User added succesfully")
+        
+        except sqlite3.Error as error:
+            debugPrint(f"Error: {error}")
+            return (False, DatabaseHandlerMessages.SQLITE_ERROR)
+        
+    
+    def getUsers(self) -> tuple[bool, str, list]:
+        """Returns a tuple: (success, message, list of users)"""
+
+        if not (self.connection and self.cursor):
+            return (False, DatabaseHandlerMessages.CONNECTION_NOT_ESTABLISHED, [])
+        
+        try:
+            return (True, DatabaseHandlerMessages.SUCCESS, self.cursor.execute("SELECT * FROM users").fetchall())
+        
+        except sqlite3.Error as error:
+            debugPrint(f"Error: {error}")
+            return (False, DatabaseHandlerMessages.SQLITE_ERROR, [])
+    
+        
